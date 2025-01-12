14a9c47e481b382726b6072e7e6da836b513117b
Shreyansh Singh <shreyansh.pettswood@gmail.com>
2025-01-12 07:53:48 -0800

    Enable checks for backwards pass

--- test_correctness.py
+++ test_correctness.py
@@ -25,7 +25,7 @@ def test_op(BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM, causal, dtype=torch.float1
     )
 
     softmax_scale = 1 / (HEAD_DIM**0.5)
-    # dO = torch.randn_like(Q)
+    dO = torch.randn_like(Q)
 
     # reference implementation
     MASK = torch.tril(torch.ones((SEQ_LEN, SEQ_LEN), device="cuda"))
@@ -34,26 +34,26 @@ def test_op(BATCH_SIZE, NUM_HEADS, SEQ_LEN, HEAD_DIM, causal, dtype=torch.float1
         P[:, :, MASK == 0] = float("-inf")
     P = torch.softmax(P.float(), dim=-1).half()
     ref_O = torch.matmul(P, V)
-    # ref_O.backward(dO)
-    # ref_dV, V.grad = V.grad.clone(), None
-    # ref_dK, K.grad = K.grad.clone(), None
-    # ref_dQ, Q.grad = Q.grad.clone(), None
+    ref_O.backward(dO)
+    ref_dV, V.grad = V.grad.clone(), None
+    ref_dK, K.grad = K.grad.clone(), None
+    ref_dQ, Q.grad = Q.grad.clone(), None
 
     # triton implementation
     tri_out = FlashAttentionTriton.apply(Q, K, V, softmax_scale, causal).half()
-    # tri_out.backward(dO)
-    # tri_dV, V.grad = V.grad.clone(), None
-    # tri_dK, K.grad = K.grad.clone(), None
-    # tri_dQ, Q.grad = Q.grad.clone(), None
+    tri_out.backward(dO)
+    tri_dV, V.grad = V.grad.clone(), None
+    tri_dK, K.grad = K.grad.clone(), None
+    tri_dQ, Q.grad = Q.grad.clone(), None
 
     # compare
     rtol = 0.0
     atol = 1e-2
     
     assert torch.allclose(ref_O, tri_out, atol=atol, rtol=rtol)
-    # assert torch.allclose(ref_dK, tri_dK, atol=atol, rtol=rtol)
-    # assert torch.allclose(ref_dV, tri_dV, atol=atol, rtol=rtol)
-    # assert torch.allclose(ref_dQ, tri_dQ, atol=atol, rtol=rtol)
+    assert torch.allclose(ref_dK, tri_dK, atol=atol, rtol=rtol)
+    assert torch.allclose(ref_dV, tri_dV, atol=atol, rtol=rtol)
+    assert torch.allclose(ref_dQ, tri_dQ, atol=atol, rtol=rtol)
 
 
 if __name__ == "__main__":
